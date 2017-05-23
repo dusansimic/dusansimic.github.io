7b6d5646a78c4ccff78b8c6d7716791f2a2f887f
Dušan Simić <dusan.simic1810@gmail.com>
2017-05-23 17:48:33 +0200

    adding responsive design

---
+++ js/onload.js
@@ -0,0 +1,8 @@
+$(document).ready(() => {
+  $('.btn-social').width($('.btn-social').height());
+  /*if ($('#body').height() > $(window).height()) {
+    $('#footer').css('position', 'relative');
+    $('#body').css('align-content', 'stretch');
+    $('#body').css('display', 'none');
+  }*/
+});
